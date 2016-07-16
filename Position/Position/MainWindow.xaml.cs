7b04d4fb20b4f2f3d5e6a0390f3ca4f42889e506
shenshumao <shenshumao@woxuwireless.com>
2016-07-16 19:24:35 +0800

    Add background port

--- Position/Position/MainWindow.xaml.cs
+++ Position/Position/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO.Ports;
 
 namespace Position
 {
@@ -20,9 +21,43 @@ namespace Position
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string[] ports;//可用串口数组
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.SystemKey == Key.LeftAlt || e.SystemKey == Key.RightAlt)
+            {
+                if (Visibility.Collapsed == Postset.Visibility)
+                {
+                    Postset.Visibility = Visibility.Visible;
+                }
+                else if (Visibility.Visible == Postset.Visibility)
+                {
+                    Postset.Visibility = Visibility.Collapsed;
+                }
+
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)//主窗口初始化
+        {
+            ports = SerialPort.GetPortNames();//获取可用串口            
+
+            if (ports.Length > 0)//ports.Length > 0说明有串口可用
+            {
+                for (int i = 0; i < ports.Length; i++)
+                {
+                    ComboBoxItem item = new ComboBoxItem();
+                    item.Content = ports[i];
+                    Portcobox.Items.Add(item);//下拉控件里添加可用串口
+                }
+                Portcobox.SelectedIndex = 0;
+            }
         }
     }
 }
